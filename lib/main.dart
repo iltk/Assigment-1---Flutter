587a244600118bce71e7c72edfcffd4b8f849f47
Illia Tkach <tkachil@sheridancollege.ca>
2026-02-03 21:56:04 -0500

    Add method that generates a random number. Add navigation from home to statistics page. CCreate statistics page that displays reset & back home btn

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 //define application colors
@@ -34,6 +36,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
+//home page
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
 
@@ -44,29 +47,49 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _randomNum = 0;
+
+  void _generateNumber() {
+    setState(() {
+      _randomNum = Random().nextInt(9) + 1;
+    });
+  }
+
+  void _viewStat() {
+    Navigator.push(
+      context,
+      MaterialPageRoute<void>(builder: (context) => const StatPage()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: bodyBackgroundColor,
       appBar: AppBar(
         backgroundColor: appBarBackgroundColor,
-        leading: Icon(Icons.home, color: textColor),
+        leading: IconButton(
+          icon: Icon(Icons.home, color: textColor),
+          onPressed: () {},
+          ),
         title: Text(widget.title, style: TextStyle(color: textColor)),
       ),
       body: Column(
         children: [
           Expanded(
             child: Center(
-              child: Text(
-                '7',
-                style: TextStyle(color: textColor, fontSize: 80),
-              ),
+              child: _randomNum == 0
+                  ? Text("Intial state")
+                  : Text(
+                      '$_randomNum',
+                      style: TextStyle(color: textColor, fontSize: 80),
+                    ),
             ),
           ),
 
           ElevatedButton(
             style: _elevatedButtonStyle,
-            onPressed: () {},
+            onPressed: _generateNumber,
             child: const Text('Generate'),
           ),
 
@@ -74,8 +97,59 @@ class _HomePageState extends State<HomePage> {
 
           ElevatedButton(
             style: _elevatedButtonStyle,
-            onPressed: () {},
+            onPressed: _viewStat,
             child: const Text('View Statistics'),
+          ),
+
+          const SizedBox(height: 20),
+        ],
+      ),
+    );
+  }
+}
+
+//statistic page
+class StatPage extends StatefulWidget {
+  const StatPage({super.key});
+
+  @override
+  State<StatPage> createState() => _StatPageState();
+}
+
+class _StatPageState extends State<StatPage> {
+  void _reset() {}
+
+  void _backToHome() {
+    Navigator.pop(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: bodyBackgroundColor,
+      appBar: AppBar(
+        backgroundColor: appBarBackgroundColor,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: textColor),
+          onPressed: _backToHome,
+          ),
+        title: Text('Statistics', style: TextStyle(color: textColor)),
+      ),
+      body: Column(
+        children: [
+        
+          ElevatedButton(
+            style: _elevatedButtonStyle,
+            onPressed: _reset,
+            child: const Text('Reset'),
+          ),
+
+          const SizedBox(height: 10),
+
+          ElevatedButton(
+            style: _elevatedButtonStyle,
+            onPressed: _backToHome,
+            child: const Text('Back to Home'),
           ),
 
           const SizedBox(height: 20),
